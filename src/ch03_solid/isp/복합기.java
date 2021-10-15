6b963bc03b32b358d3eb27c1a481d9a80aae7436
ohj4312 <ohj4312@naver.com>
2021-10-16 03:27:45 +0900

    isp 위배된 설계

---
+++ src/ch03_solid/isp/복합기.java
@@ -0,0 +1,13 @@
+package ch03_solid.isp;
+
+public class 복합기{
+    public void copy(){
+        System.out.println("복사");
+    }
+    public void fax() {
+        System.out.println("팩스");
+    }
+    public void print () {
+        System.out.println("프린터");
+    }
+}
