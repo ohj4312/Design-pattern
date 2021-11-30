2d67477db641e76bb6c9c9a1465f6d28cf70a9b7
ohj4312 <ohj4312@naver.com>
2021-11-30 23:48:53 +0900

    out package upload

--- src/ch09_observer/practice1/Battery.java
+++ src/ch09_observer/practice1/Battery.java
@@ -4,21 +4,21 @@ package ch09_observer.practice1;
 //배터리 잔량에 변경이 생기면 BatteryLevellDisplay 클래스와 LowBatteryWarning 클래스에게 배터리 잔량이 변경되었음을 알린다.
 public class Battery {
     private int level=100;
-    private BatteryLevelDisplay display;
-    private LowBatteryWarning wraning;
+    private BatteryLevelDisplay display; //OCP 위반
+    private LowBatteryWarning wraning; //OCP 위반
 
     public void setDisplay(BatteryLevelDisplay display){
         this.display=display;
-    }
+    } //OCP위반
 
     public void setWraning(LowBatteryWarning wraning){
         this.wraning=wraning;
-    }
+    } //OCP 위반
 
     public void consume(int amount){
         level-=amount;
-        display.update();
-        wraning.update();
+        display.update(); //OCP 위반
+        wraning.update(); //OCP 위반
     }
 
     public int getLevel(){
